2e47e86591b74a6886143c3fdae96eb4e72e3107
araya <araya@araya.dev>
2023-09-17 01:13:24 +0900

    Parse cli args using clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long, default_value = "madoka.conf.yaml")]
+    config: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("config file: {}", args.config)
 }
