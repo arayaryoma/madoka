a54b882334e73bb72e50c3cf3dde89fc26a5c212
araya <araya@araya.dev>
2023-09-18 16:31:11 +0900

    Parse config file as yaml

---
+++ Makefile
@@ -0,0 +1,10 @@
+.PHONY: build
+build:
+	@echo "Building..."
+	cargo build
+
+.PHONY: install
+install: build
+	@echo "Installing..."
+	cp target/debug/madoka /usr/local/bin/madoka
+	
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use serde::Deserialize;
+use std::fs::File;
+use std::io::Read;
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -6,7 +10,32 @@ struct Args {
     config: String,
 }
 
+#[derive(Debug, Deserialize)]
+struct Config {
+    port: u16,
+}
+
 fn main() {
     let args = Args::parse();
-    println!("config file: {}", args.config)
+    let config_path = args.config;
+
+    let mut file = match File::open(config_path) {
+        Ok(file) => file,
+        Err(e) => panic!("Error opening config file: {}", e),
+    };
+
+    let mut contents = String::new();
+    match file.read_to_string(&mut contents) {
+        Ok(_) => (),
+        Err(e) => panic!("Error reading config file: {}", e),
+    };
+
+    let config: Config = match serde_yaml::from_str(&mut contents) {
+        Ok(config) => config,
+        Err(e) => panic!("Error parsing config file: {}", e),
+    };
+
+    let port = config.port;
+
+    println!("Listening on port {}", port);
 }
