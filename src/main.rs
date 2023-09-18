f0e7856d3b7c4648a0bf766f0363c7bf19c80a65
araya <araya@araya.dev>
2023-09-18 18:24:37 +0900

    Implement responsing `Hello, World!` from web server

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use serde::Deserialize;
 use std::fs::File;
 use std::io::Read;
+use std::net::SocketAddr;
 
+use bytes::Bytes;
 use clap::Parser;
+use http_body_util::Full;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper_util::rt::TokioIo;
+use serde::Deserialize;
+use tokio::net::TcpListener;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -15,7 +22,8 @@ struct Config {
     port: u16,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let config_path = args.config;
 
@@ -37,5 +45,23 @@ fn main() {
 
     let port = config.port;
 
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+
+    let listener = TcpListener::bind(addr).await?;
     println!("Listening on port {}", port);
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new().serve_connection(io, service_fn(service)).await {
+                eprintln!("Failed to serve connection: {}", err);
+            }
+        });
+    }
+}
+async fn service(
+    _req: hyper::Request<hyper::body::Incoming>,
+) -> Result<hyper::Response<Full<Bytes>>, hyper::Error> {
+    let body = Full::new(Bytes::from("Hello, World!"));
+    Ok(hyper::Response::new(body))
 }
